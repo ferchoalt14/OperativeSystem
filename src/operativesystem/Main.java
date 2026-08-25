80b3abc6c14b45cf98cb6b167b54c40933b8b0b2
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-25 08:01:27 -0600

    test commit

--- src/operativesystem/Main.java
+++ src/operativesystem/Main.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package operativesystem;
 
 /**
  *
- * @author User
+ * @author leandro
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pilinga");
+        System.out.println("Pilinga ferchooo te amoooooo ");
     }
 }
